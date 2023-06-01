128ee6241287a94b93ce1e3eddc8e4140830361c
Devfritz <devfritzcode@gmail.com>
2023-06-01 14:58:37 -0400

    update component imagemultiple questions

--- App.js
+++ App.js
@@ -4,46 +4,41 @@ import OptionCard from "./components/OptionCard";
 import question from "./assets/data/imageMulatipleChoiceQuestions";
 import { useEffect, useState } from "react";
 import ButtonCheck from "./components/Button";
+import ImageMultipleChoiceQuestion from "./components/imageMultipleChoiceQuestion/ImageMultipleChoiceQuestion";
 export default function App() {
   const [selected, setSelected] = useState(null);
   const [questionIndex, setQuestionIndex] = useState(0);
-  const [currentQuestion, seCurrentQuestion] = useState(
+  const [currentQuestion, setCurrentQuestion] = useState(
     question[questionIndex]
   );
 
-  useEffect(() => {
-    console.log(currentQuestion);
+  const winAlert = () => {
     if (questionIndex >= question.length) {
       Alert.alert("Win WIN WIN");
       setQuestionIndex(0);
     } else {
-      seCurrentQuestion(question[questionIndex]);
+      setCurrentQuestion(question[questionIndex]);
     }
-  }, [questionIndex]);
+  };
 
   const checkAnswer = () => {
     if (selected.correct) {
       setQuestionIndex(questionIndex + 1);
-    } else {
-      Alert.alert("Not correct");
     }
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{currentQuestion.question}</Text>
-      <View style={styles.optionContainer}>
-        {currentQuestion.options.map((option) => (
-          <OptionCard
-            key={option.id}
-            name={option.text}
-            image={option.image}
-            onPress={() => setSelected(option)}
-            isSelected={selected?.id === option.id}
-          />
-        ))}
-      </View>
-      <ButtonCheck onPress={() => checkAnswer()} />
+      <ImageMultipleChoiceQuestion
+        selected={selected}
+        setSelected={setSelected}
+        currentQuestion={currentQuestion}
+        setCurrentQuestion={setCurrentQuestion}
+        questionIndex={questionIndex}
+        setQuestionIndex={setQuestionIndex}
+        checkAnswer={checkAnswer}
+        winAlert={winAlert}
+      />
     </View>
   );
 }
@@ -56,21 +51,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginTop: 60,
     padding: 6,
-  },
-  text: {
-    fontSize: 22,
-    fontWeight: "bold",
-    color: "#2c2c2c",
-    alignSelf: "stretch",
-    marginBottom: 10,
-  },
-
-  optionContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    flex: 1,
-    justifyContent: "space-between",
-    alignContent: "space-between",
-    width: "100%",
   },
 });
---
+++ components/imageMultipleChoiceQuestion/ImageMultipleChoiceQuestion.js
@@ -0,0 +1,55 @@
+import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import ButtonCheck from "../Button";
+import OptionCard from "../OptionCard";
+
+const ImageMultipleChoiceQuestion = ({
+  selected,
+  setSelected,
+  currentQuestion,
+  questionIndex,
+  checkAnswer,
+  winAlert,
+}) => {
+  useEffect(() => {
+    winAlert();
+  }, [questionIndex]);
+  return (
+    <>
+      <Text style={styles.text}>{currentQuestion.question}</Text>
+      <View style={styles.optionContainer}>
+        {currentQuestion.options.map((option) => (
+          <OptionCard
+            key={option.id}
+            name={option.text}
+            image={option.image}
+            onPress={() => setSelected(option)}
+            isSelected={selected?.id === option.id}
+          />
+        ))}
+      </View>
+      <ButtonCheck onPress={() => checkAnswer()} />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#2c2c2c",
+    alignSelf: "stretch",
+    marginBottom: 10,
+  },
+
+  optionContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    flex: 1,
+    justifyContent: "space-between",
+    alignContent: "space-between",
+    width: "100%",
+  },
+});
+
+export default ImageMultipleChoiceQuestion;
